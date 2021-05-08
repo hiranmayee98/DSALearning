f0551ec813eb85f205ce6f86b8d035ff43534076
Hiranmayee Das <53131453+hiranmayee98@users.noreply.github.com>
2021-05-08 18:30:56 +0530

    Update REVMEE.py

--- REVMEE.py
+++ REVMEE.py
@@ -1,7 +1,7 @@
 #You are given a list of N integers and you need to reverse it and print the reversed list in a new line
 
-
 i=int(input())
-l=list(map(int,input().split(" ")))
+l=list(map(str,input().split(" ")))
 l.reverse()
-print(l)
+st=" "
+print(st.join(l))

50713137889f83ef8bf04a500886cbdecfa9f3ad
Hiranmayee Das <53131453+hiranmayee98@users.noreply.github.com>
2021-05-09 16:04:49 +0530

    Update TRIVALCH.py

--- TRIVALCH.py
+++ TRIVALCH.py
@@ -1,6 +1,9 @@
 '''
-
-
+You're given the length of three sides a, b, and c respectively.
+Now check if these three sides can form a triangle or not. 
+Print "YES"(without quotes) if it can form a valid triangle with an area greater than 0, 
+otherwise print "NO" (without quotes).
+'''
 
 import math
 try :
